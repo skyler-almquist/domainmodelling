5634c2abe489401d5f3b021d2bbf6dd21fbd1124
Skyler Almquist <skyler.almquist@kellogg.northwestern.edu>
2023-04-14 20:53:14 +0000

    in class phase 1

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,50 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
--- e.g. DROP TABLE IF EXISTS ______;
 
--- CREATE TABLES
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS salesperson_staff;
+DROP TABLE IF EXISTS industry;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS industry_membership;
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  title TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE company (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_name TEXT
+);
+
+CREATE TABLE salesperson_staff (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE industry (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  notes TEXT,
+  created_at TEXT,
+  salesperson_staff_id INTEGER,
+  contacts_id INTEGER
+);
+
+CREATE TABLE industry_membership (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
+    industry_id INTEGER
+);
